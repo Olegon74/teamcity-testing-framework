f4177dc4ed41ff24960954d970096b21456a7f0e
Oleg <olegka-orehov@yandex.ru>
2024-11-20 11:41:32 +0500

    Add swagger coverage

--- src/main/java/com/example/teamcity/api/spec/Specifications.java
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -2,11 +2,17 @@ package com.example.teamcity.api.spec;
 
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.models.User;
+import com.github.viclovsky.swagger.coverage.FileSystemOutputWriter;
+import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import java.nio.file.Paths;
+
+import static org.testng.CommandLineArgs.OUTPUT_DIRECTORY;
 
 public class Specifications {
 
@@ -14,6 +20,11 @@ public class Specifications {
         var requestBuilder = new RequestSpecBuilder();
         requestBuilder.addFilter(new RequestLoggingFilter());
         requestBuilder.addFilter(new ResponseLoggingFilter());
+        requestBuilder.addFilter(new SwaggerCoverageRestAssured(
+                new FileSystemOutputWriter(
+                        Paths.get("target/" + OUTPUT_DIRECTORY)
+                )
+        ));
         requestBuilder.setContentType(ContentType.JSON);
         requestBuilder.setAccept(ContentType.JSON);
         return requestBuilder;
@@ -36,5 +47,6 @@ public class Specifications {
         return requestBuilder.build();
     }
 }
+
 
 
